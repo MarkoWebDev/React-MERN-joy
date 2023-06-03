87b99cfa122be7443a3008110ed606cf346c3711
MarkoPolo125 <markobeg15@gmail.com>
2023-06-04 01:29:40 +0200

    client form validations

--- client/app/src/Pages/Register.tsx
+++ client/app/src/Pages/Register.tsx
@@ -9,17 +9,12 @@ import {
   InputRightElement,
   Button,
   Checkbox,
-  CheckboxGroup,
   Stack,
 } from "@chakra-ui/react";
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
+import { FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
 import char1 from "../assets/images/char1.jpg";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 interface RegisterProps {}
 
@@ -35,59 +30,112 @@ const Register: FC<RegisterProps> = () => {
     name: "",
     email: "",
     password: "",
-    checked: false,
+    checked: true,
   };
 
-  const validate = (values: any) => {};
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, "Name must be at least 3 characters long")
+      .max(15, "Name is too long")
+      .required("Name is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .matches(
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+        "Invalid email address"
+      )
+      .required("Email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Must be at least 8 characters")
+      .matches(/[a-zA-Z]/, "Password can onyl contain letters"),
+    checked: Yup.boolean().oneOf([true], "This field must be checked"),
+  });
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement> | any) => {
     e.preventDefault();
     console.log("formik", formik);
+    console.log("obj", Object.keys(formik.errors).length);
   };
 
-  const formik = useFormik({ initialValues, onSubmit, validate });
+  const formik = useFormik({ initialValues, onSubmit, validationSchema });
 
   const [show, setShow] = useState<boolean>(false);
   const handleClick = () => setShow(!show);
+
   return (
-    <Box className="flex items-center justify-center h-screen bg-slate-700">
+    <Box className="flex items-center justify-center h-screen bg-slate-100">
       <Box className="grid grid-cols-2 w-[950px] bg-slate-200 rounded-lg ">
         <Box className="flex flex-col w-full pl-4">
           <Heading as="h2" size="lg" className="py-4">
             Register 🔐
           </Heading>
           <Text fontSize="md" className="text-gray-400">
-            Join our TEAM and let's build something awsome. Register and
-            procceed to the next step
+            Join our TEAM and let's build something awesome. Register and
+            procceed to the next step.
           </Text>
           <Box className="py-2">
             <form onSubmit={onSubmit}>
               <FormControl>
                 <FormLabel>First name</FormLabel>
                 <Input
+                  isInvalid={
+                    formik.touched.name && formik.errors.name ? true : false
+                  }
+                  errorBorderColor="crimson"
+                  focusBorderColor="gray"
+                  id="name"
                   type="text"
                   placeholder="Enter your First name"
                   variant="outline"
-                  focusBorderColor="gray.400"
+                  {...formik.getFieldProps("name")}
                 />
+                {formik.touched.name && formik.errors.name && (
+                  <FormHelperText color="crimson">
+                    {formik.errors.name}
+                  </FormHelperText>
+                )}
               </FormControl>
+
+              {/* Email */}
               <FormControl isRequired className="py-2">
                 <FormLabel>Email address</FormLabel>
                 <Input
+                  isInvalid={
+                    formik.touched.email && formik.errors.email ? true : false
+                  }
+                  errorBorderColor="crimson"
+                  focusBorderColor="gray"
+                  id="email"
                   type="email"
                   variant="outline"
-                  focusBorderColor="gray.400"
                   placeholder="Enter your email address"
+                  {...formik.getFieldProps("email")}
                 />
                 <FormHelperText>We'll never share your email.</FormHelperText>
+                {formik.touched.email && formik.errors.email && (
+                  <FormHelperText color="crimson">
+                    {formik.errors.email}
+                  </FormHelperText>
+                )}
               </FormControl>
+              {/* Password */}
               <FormControl isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup size="md">
                   <Input
+                    isInvalid={
+                      formik.touched.password && formik.errors.password
+                        ? true
+                        : false
+                    }
+                    errorBorderColor="crimson"
+                    focusBorderColor="gray"
+                    id="password"
                     pr="4.5rem"
                     type={show ? "text" : "password"}
                     placeholder="Enter password"
+                    {...formik.getFieldProps("password")}
                   />
                   <InputRightElement width="4.5rem">
                     <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -95,14 +143,45 @@ const Register: FC<RegisterProps> = () => {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
+                {formik.touched.password && formik.errors.password && (
+                  <FormHelperText color="crimson">
+                    {formik.errors.password}
+                  </FormHelperText>
+                )}
               </FormControl>
-              <Stack className="pt-4" spacing={5} direction="row">
-                <Checkbox colorScheme="blue" defaultChecked>
-                  <Text fontSize="sm"> Accept Terms of Agreement</Text>
-                </Checkbox>
-              </Stack>
+              {/* Checkbox */}
+              <FormControl>
+                <Stack className="pt-4" spacing={5} direction="row">
+                  <Checkbox
+                    defaultChecked
+                    id="checked"
+                    colorScheme="blue"
+                    isInvalid={formik.errors.checked ? true : false}
+                    {...formik.getFieldProps("checked")}
+                  >
+                    <Text fontSize="sm"> Accept Terms of Agreement</Text>
+                  </Checkbox>
+                </Stack>
+                {formik.touched.checked && formik.errors.checked && (
+                  <FormHelperText color="crimson">
+                    {formik.errors.checked}
+                  </FormHelperText>
+                )}
+              </FormControl>
               <Box className="py-4">
-                <Button type="submit" colorScheme="teal" variant="ghost">
+                <Button
+                  type="submit"
+                  colorScheme="teal"
+                  variant="solid"
+                  isDisabled={
+                    Object.keys(formik.errors).length > 0 ||
+                    formik.values.name === "" ||
+                    formik.values.email === "" ||
+                    formik.values.password === ""
+                      ? true
+                      : false
+                  }
+                >
                   Register
                 </Button>
               </Box>
